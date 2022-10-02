c8f5d0e284d5b9d412fcc337467ff53470c8693a
Mat <matgallimore@romainberg.com>
2022-10-02 16:02:08 +0100

    feat(master) More advanced Mongo Controlling and routing

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,19 @@
-export const getPosts = (req, res, () =>{
-    res.send("THIS WORKS");
-})
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+    } catch (error) {
+        
+    }
+}
+
+export const createPost = async (req, res) => {
+    res.send("Post creation");
+}
+
+
+
+
+
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import { createPost, getPosts } from '../controllers/posts.js';
+
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("This worked!");
-})
+router.get('/', getPosts);
+router.post('/', createPost);
+
 
 export default router;
